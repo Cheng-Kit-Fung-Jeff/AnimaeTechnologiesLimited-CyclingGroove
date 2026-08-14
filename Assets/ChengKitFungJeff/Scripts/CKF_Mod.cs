e7cf0972c032f7cb2ac325cfe30ddbf8437b8cbe
Cheng-Kit-Fung-Jeff <jeffcheng199906@gmail.com>
2026-08-14 08:14:38 +0800

    Added reset indicator

--- Assets/ChengKitFungJeff/Scripts/CKF_Mod.cs
+++ Assets/ChengKitFungJeff/Scripts/CKF_Mod.cs
@@ -8,23 +8,33 @@ public class CKF_Mod : MonoBehaviour
     public float value, modulus;
     public UnityEvent<float> getValue = new();
 
-    public void SetValue(float value) { this.value = value % modulus; }
+    public void SetValue(float value)
+    {
+        this.value = value % modulus;
+        if (this.value < 0) this.value += Mathf.Abs(modulus);
+    }
 
-    public void Increment(float value) { this.value = (this.value+value) % modulus; }
+    public void Increment(float value)
+    {
+        this.value = (this.value + value) % modulus;
+        if (this.value < 0) this.value += Mathf.Abs(modulus);
+    }
     public void GetIncrement(float value)
     {
         this.value = (this.value + value) % modulus;
+        if (this.value < 0) this.value += Mathf.Abs(modulus);
         getValue?.Invoke(this.value);
     }
 
     public void GetValue(float value)
     {
         this.value = value % modulus;
+        if (this.value < 0) this.value += Mathf.Abs(modulus);
         getValue?.Invoke(this.value);
     }
 
     public void GetValue()
     {
-        getValue?.Invoke(this.value);
+        getValue?.Invoke(value);
     }
 }
